447c0227e21fbcb85acb6759235d4005b42f201d
OnlyAHuman7926 <149751445+OnlyAHuman7926@users.noreply.github.com>
2024-06-20 04:38:21 +0000

    Added even more color

--- color.js
+++ color.js
@@ -1,5 +1,25 @@
 let startHue = Math.random() * 360;
 let endHue = (startHue + (Math.random() * 45 + 45) * (Math.random() > 0.5 ? 1 : -1));
-let brightness = Math.random() * 15 + 55;
+let brightness1 = Math.random() * 15 + 55;
+let brightness2 = Math.random() * 15 + 55;
 
-document.querySelector(".sidebar").style.background = `linear-gradient(180deg, hsl(${startHue}, 100%, ${brightness}%), hsl(${endHue}, 100%, ${brightness}%)`;
+let v = [1, 1];
+let accel = [0, 0];
+
+let prev = new Date();
+requestAnimationFrame(function f() {
+  let now = new Date();
+  let d = (now - prev) / 1000;
+  prev = now;
+
+  accel = [accel[0] + (Math.random() * 2 - 1) * 0.01, accel[1] + (Math.random() * 2 - 1) * 0.01];
+  v = [v[0] + d * accel[0], v[1] + d * accel[1]];
+  startHue += d * v[0];
+  endHue += d * v[1];
+
+  document.querySelector(".sidebar").style.background = `linear-gradient(180deg, 
+  hsl(${startHue}, 100%, ${brightness1}%), 
+  hsl(${endHue}, 100%, ${brightness2}%)`;
+
+  requestAnimationFrame(f);
+})
