fe1fbc56362e934b9a9240b2bf6a802c174490f2
OnlyAHuman7926 <149751445+OnlyAHuman7926@users.noreply.github.com>
2024-07-03 12:01:20 +0000

    Finalized color settings using my random smooth motion model at Replit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Clock
-A clock made for examinations. 
+A clock made for examinations.
+To use, follow the steps on the screen. 
+
+An options page and languages may come in the future. 
--- color.js
+++ color.js
@@ -1,25 +1,83 @@
-let startHue = Math.random() * 360;
-let endHue = (startHue + (Math.random() * 45 + 45) * (Math.random() > 0.5 ? 1 : -1));
-let brightness1 = Math.random() * 15 + 55;
-let brightness2 = Math.random() * 15 + 55;
+class Motion {
+  constructor(level, f) {
+    // Creates a motion curve that has f as its derivative at the given level
+    this.vals = new Array(level + 1).fill(0);
+    /* this.elem = {};
+    this.elem.ball = document.createElement("div");
+    this.elem.ball.classList.add("ball");
+    this.elem.disp = document.createElement("div");
+    this.elem.container = document.createElement("div");
+    this.elem.container.append(this.elem.ball, this.elem.disp);
+    document.getElementById('container').append(this.elem.container); */
+    this.f = f;
+    f.bind(this);
+  }
+  update(t, dt) {
+    // Run f for time t and update motion
+    this.vals[this.vals.length - 1] = this.f(t);
+    for (let i = this.vals.length - 2; i >= 0; i--) {
+      this.vals[i] += this.vals[i + 1] * dt;
+    }
+  }
+  disp() {
+    // this.elem.ball.style.marginLeft = this.vals[0] + "px";
+  }
+}
 
-let v = [1, 1];
-let accel = [0, 0];
+let currentDir = 1, cd2 = 1;
+function fff() {
+  let currentAccel = this.vals[2] ?? 0;
+  if (this.vals[1] > 20 || currentAccel > 5) {
+    this.vals[1] = Math.min(this.vals[1], 20);
+    currentAccel = -1;
+    currentDir = -1;
+  } else if (this.vals[1] < -20 || currentAccel < -5) {
+    this.vals[1] = Math.max(this.vals[1], -20);
+    currentAccel = 1;
+    currentDir = 1;
+  } else if (Math.random() < 1 / 90) {
+    let otherThing = Math.random();
+    if (otherThing > 0.7) currentDir = -currentDir;
+    currentAccel += currentDir;
 
-let prev = new Date();
+  }
+  return currentAccel;
+}
+function g() {
+  let currentAccel = this.vals[2] ?? 0;
+  if (this.vals[1] > 20 || currentAccel > 5) {
+    this.vals[1] = Math.min(this.vals[1], 20);
+    currentAccel = -1;
+    cd2 = -1;
+  } else if (this.vals[1] < -20 || currentAccel < -5) {
+    this.vals[1] = Math.max(this.vals[1], -20);
+    currentAccel = 1;
+    cd2 = 1;
+  } else if (Math.random() < 1 / 90) {
+    let otherThing = Math.random();
+    if (otherThing > 0.7) cd2 = -cd2;
+    currentAccel += cd2;
+  }
+  return currentAccel;
+}
+
+let motion = new Motion(2, fff);
+let motion2 = new Motion(2, g);
+let initial = Math.random() * 360;
+let offset = Math.random() * 30 + 30;
+
+let start = new Date();
+let before = start;
 requestAnimationFrame(function f() {
   let now = new Date();
-  let d = (now - prev) / 1000;
-  prev = now;
-
-  accel = [accel[0] + (Math.random() * 2 - 1) * 0.04, accel[1] + (Math.random() * 2 - 1) * 0.04];
-  v = [v[0] + d * accel[0], v[1] + d * accel[1]];
-  startHue += d * v[0];
-  endHue += d * v[1];
-
+  let dt = (now - before) / 1000;
+  before = now;
+  motion.update((now - start) / 1000, dt);
+  motion2.update((now - start) / 1000, dt);
+  //document.body.style.backgroundColor = `hsl(${motion.vals[0] * 1}, 100%, 70%)`;
   document.querySelector(".sidebar").style.background = `linear-gradient(180deg, 
-  hsl(${startHue}, 100%, ${brightness1}%), 
-  hsl(${endHue}, 100%, ${brightness2}%)`;
-
+  hsl(${initial + motion.vals[0]}, 100%, 80%), 
+  hsl(${initial + motion2.vals[0] + offset}, 100%, 60%)`;
+  console.log(motion.vals[1].toFixed(2), motion.vals[2].toFixed(2), motion2.vals[1].toFixed(2), motion2.vals[2].toFixed(2));
   requestAnimationFrame(f);
 })
